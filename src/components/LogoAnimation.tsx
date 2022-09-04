964a7a82309efbdf8fb115d952a1f38f192434cf
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-04 17:41:14 +0530

    Refactor: Comments

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -3,10 +3,6 @@ import ReactLoading from 'react-loading';
 /**
  * Adapted from react-loading
  * Refer: https://gsithub.com/fakiolinho/react-loading#readme
- * Loading animation types used here:
- * 1. bars
- * 2. bubbles
- * 3. cylon
  */
 export const LogoAnimation = () => {
   return <ReactLoading type={'bubbles'} color={'#4db5ff'} />;
