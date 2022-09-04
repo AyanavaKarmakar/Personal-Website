fe53e7c2704d69ffb91e682b9797c34ea5fa5e48
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-05 02:19:00 +0530

    .

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Paper, TextField, Typography } from '@mui/material';
 import { TypeAnimation } from 'react-type-animation';
 
 interface Props {
@@ -15,41 +15,77 @@ interface Props {
 export const Home = (props: Props) => {
   const { darkMode } = props;
   return (
-    <Box
-      sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%,-50%)',
-        width: '100vw',
-        textAlign: 'center',
-        zIndex: 1,
-      }}
-    >
-      <Typography variant="h4" color={`${darkMode === true ? 'secondary' : 'primary'}`}>
-        <TypeAnimation sequence={['Ayanava Karmakar', 1000]} speed={50} wrapper="span" repeat={0} />
-      </Typography>
-      <Typography variant="h5" color={`${darkMode === true ? 'secondary' : 'primary'}`}>
-        <TypeAnimation
-          sequence={[
-            'Frontend Developer',
-            1000,
-            'Frontend Developer | Technical Content Writer',
-            1000,
-          ]}
-          speed={10}
-          wrapper="span"
-          repeat={Infinity}
+    <Paper>
+      <Paper sx={{ width: 1, height: 1 }}>
+        <img src="/Cat.jpg" alt="white_cat" height="100%" width="100%" />
+      </Paper>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+          width: '100vw',
+          textAlign: 'center',
+          zIndex: 1,
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          color="secondary"
+          variant="outlined"
+          defaultValue={}
         />
-      </Typography>
-      <Typography variant="h6" color={`${darkMode === true ? 'secondary' : 'primary'}`}>
-        <TypeAnimation
-          sequence={['Based in Kolkata', 1000, 'Based in Kolkata, India', 1000]}
-          speed={1}
-          wrapper="span"
-          repeat={0}
-        />
-      </Typography>
-    </Box>
+        <Typography
+          variant="h4"
+          color={`${darkMode === true ? 'secondary' : '#000000'}`}
+          sx={{ fontWeight: 'bold' }}
+        >
+          <TypeAnimation
+            sequence={['Ayanava Karmakar', 1000]}
+            speed={50}
+            wrapper="span"
+            repeat={0}
+          />
+        </Typography>
+        <Typography
+          variant="h5"
+          color={`${darkMode === true ? 'secondary' : '#00FFFF'}`}
+          sx={{ fontWeight: 'medium' }}
+        >
+          <TypeAnimation
+            sequence={['Frontend Developer &', 1000]}
+            speed={5}
+            wrapper="span"
+            repeat={Infinity}
+          />
+        </Typography>
+        <Typography
+          variant="h5"
+          color={`${darkMode === true ? 'secondary' : '#00FFFF'}`}
+          sx={{ fontWeight: 'medium' }}
+        >
+          <TypeAnimation
+            sequence={['Technical Content Writer', 1000]}
+            speed={5}
+            wrapper="span"
+            repeat={Infinity}
+          />
+        </Typography>
+        <Typography
+          variant="h6"
+          color={`${darkMode === true ? 'secondary' : '#00FFFF'}`}
+          sx={{ fontWeight: 'light' }}
+        >
+          <TypeAnimation
+            sequence={['Based in Kolkata', 1000, 'Based in Kolkata, India', 1000]}
+            speed={1}
+            wrapper="span"
+            repeat={0}
+          />
+        </Typography>
+      </Box>
+    </Paper>
   );
 };
