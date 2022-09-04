362e9bf3bd807fd01cbb8f17de87fa521123da21
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-05 01:29:14 +0530

    Feat: Setup basic styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,9 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { TypeAnimation } from 'react-type-animation';
+
+interface Props {
+  darkMode: boolean;
+}
 
 /**
  * Refer: https://www.npmjs.com/package/react-type-animation
@@ -8,41 +12,44 @@ import { TypeAnimation } from 'react-type-animation';
  * Animation will be rendered as a <span>
  * Repeat this Animation Sequence infinitely
  */
-export const Home = () => {
+export const Home = (props: Props) => {
+  const { darkMode } = props;
   return (
-    <>
-      <Box sx={{ width: '50em' }}>
-        <TypeAnimation
-          sequence={['Ayanava Karmakar', 1000]}
-          speed={50}
-          style={{ fontSize: '2em' }}
-          wrapper="span"
-          repeat={0}
-        />
-      </Box>
-      <Box sx={{ width: '50em' }}>
+    <Box
+      sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)',
+        width: '100vw',
+        textAlign: 'center',
+        zIndex: 1,
+      }}
+    >
+      <Typography variant="h4" color={`${darkMode === true ? 'secondary' : 'primary'}`}>
+        <TypeAnimation sequence={['Ayanava Karmakar', 1000]} speed={50} wrapper="span" repeat={0} />
+      </Typography>
+      <Typography variant="h5" color={`${darkMode === true ? 'secondary' : 'primary'}`}>
         <TypeAnimation
           sequence={[
             'Frontend Developer',
             1000,
-            'Frontend Developer | Technical Content Writer.',
+            'Frontend Developer | Technical Content Writer',
             1000,
           ]}
           speed={10}
-          style={{ fontSize: '2em' }}
           wrapper="span"
           repeat={Infinity}
         />
-      </Box>
-      <Box sx={{ width: '50em' }}>
+      </Typography>
+      <Typography variant="h6" color={`${darkMode === true ? 'secondary' : 'primary'}`}>
         <TypeAnimation
           sequence={['Based in Kolkata', 1000, 'Based in Kolkata, India', 1000]}
           speed={1}
-          style={{ fontSize: '2em' }}
           wrapper="span"
           repeat={0}
         />
-      </Box>
-    </>
+      </Typography>
+    </Box>
   );
 };
