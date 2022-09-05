86a0b813308346b058251f511b4ed2681f39b241
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-05 23:49:28 +0530

    Feat: setup `right` transition

--- src/components/TimeLineTransition.tsx
+++ src/components/TimeLineTransition.tsx
@@ -1,6 +1,33 @@
+import { Box, FormControlLabel, Switch, Slide } from '@mui/material';
+import { useState } from 'react';
+import { TimeLine } from './TimeLine';
+
+interface Props {
+  darkMode: boolean;
+}
+
 /**
  * Ref: https://mui.com/material-ui/transitions/#slide
  */
-export const TimeLineTransition = () => {
-  return <>Stuff</>;
+export const TimeLineTransition = (props: Props) => {
+  const { darkMode } = props;
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(prev => !prev);
+  };
+
+  return (
+    <Box sx={{ height: 180 }}>
+      <Box sx={{ width: `calc(100px + 16px)` }}>
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleChange} />}
+          label="Show"
+        />
+        <Slide direction="right" in={checked} mountOnEnter unmountOnExit>
+          <TimeLine darkMode={darkMode} />
+        </Slide>
+      </Box>
+    </Box>
+  );
 };
