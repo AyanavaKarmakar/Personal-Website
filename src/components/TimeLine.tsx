aa096753a017e23b04271e88731138153d8e3f76
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-05 15:23:08 +0530

    Feat: Added academic details

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -9,44 +9,99 @@ import {
   TimelineContent,
   TimelineDot,
   TimelineItem,
-  TimelineSeparator,
   TimelineOppositeContent,
+  TimelineSeparator,
 } from '@mui/lab';
 import { Typography } from '@mui/material';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
+import { Code, School } from '@mui/icons-material';
 
+/**
+ * Refer: https://mui.com/material-ui/react-timeline/
+ */
 export const TimeLine = () => {
   return (
-    <Timeline position="left">
+    <Timeline position="right">
       <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography color="secondary" sx={{ fontStyle: 'italic' }}>
+            August, 2022
+          </Typography>
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineDot />
+          <TimelineDot>
+            <Code fontSize="small" />
+          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>
+          <Typography color="secondary" variant="h6">
+            Technical Content Writer
+          </Typography>
+          <Typography color="secondary">GeeksForGeeks</Typography>
+          <Typography color="secondary">Intern</Typography>
+        </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography color="secondary" sx={{ fontStyle: 'italic' }}>
+            2018 — 2022
+          </Typography>
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineDot />
+          <TimelineDot>
+            <School fontSize="small" />
+          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
+        <TimelineContent>
+          <Typography color="secondary" variant="h6">
+            Undergraduate
+          </Typography>
+          <Typography color="secondary">MAKAUT / WBUT</Typography>
+          <Typography color="secondary">Bachelor of Technology</Typography>
+          <Typography color="secondary">Electrical Engineering</Typography>
+          <Typography color="secondary">GPA: 8.7 / 10</Typography>
+        </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography color="secondary" sx={{ fontStyle: 'italic' }}>
+            2017 — 2018
+          </Typography>
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineDot />
+          <TimelineDot>
+            <School fontSize="small" />
+          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
+        <TimelineContent>
+          <Typography color="secondary" variant="h6">
+            Higher Secondary
+          </Typography>
+          <Typography color="secondary">CBSE</Typography>
+          <Typography color="secondary">Stream: PCMCs</Typography>
+          <Typography color="secondary">Percentage: 87.4%</Typography>
+        </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography color="secondary" sx={{ fontStyle: 'italic' }}>
+            2015 — 2016
+          </Typography>
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineDot />
+          <TimelineDot>
+            <School fontSize="small" />
+          </TimelineDot>
         </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
+        <TimelineContent>
+          <Typography color="secondary" variant="h6">
+            Secondary
+          </Typography>
+          <Typography color="secondary">CBSE</Typography>
+          <Typography color="secondary">GPA: 9.3 / 10</Typography>
+        </TimelineContent>
       </TimelineItem>
     </Timeline>
   );
