ee49d62b7e1827a4e2a413d6dfcf746c025b6804
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-06 21:01:23 +0530

    feat: import `useNavigate`

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,3 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+
+/**
+ *
+ * Refer = https://reactrouter.com/en/main/hooks/use-navigate#usenavigate
+ */
 export const LandingPage = () => {
   return <>LandingPage</>;
 };
