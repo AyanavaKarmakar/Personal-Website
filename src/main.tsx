1881e97cd104bb35d8918319c1bd83955ff4595a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-07 20:45:13 +0530

    feat: import css file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { HashRouter } from 'react-router-dom';
+import '../styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
