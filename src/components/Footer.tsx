9df3dbeb4aec5921a0dc21a96c011f997f98e1fd
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-07 23:52:05 +0530

    feat: removed labels

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,24 @@
 import { useState } from 'react';
 import { Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { Restore, Favorite, LocationOn } from '@mui/icons-material';
+import { Restore, Favorite, LocationOn, Folder } from '@mui/icons-material';
 
 /**
  * Refer = https://mui.com/material-ui/react-bottom-navigation/#bottom-navigation
  */
 export const Footer = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState('recents');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   return (
-    <Box
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-      minWidth="100vw"
-      sx={{ width: 500 }}
-    >
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<Restore />} />
-        <BottomNavigationAction label="Favorites" icon={<Favorite />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOn />} />
+    <Box justifyContent="center" alignItems="center" minHeight="100vh" minWidth="100vw">
+      <BottomNavigation value={value} onChange={handleChange}>
+        <BottomNavigationAction label="Recents" value="recents" icon={<Restore />} />
+        <BottomNavigationAction label="Favorites" value="favorites" icon={<Favorite />} />
+        <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOn />} />
+        <BottomNavigationAction label="Folder" value="folder" icon={<Folder />} />
       </BottomNavigation>
     </Box>
   );
