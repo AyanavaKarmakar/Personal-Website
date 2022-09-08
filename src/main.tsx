a2c2002316c87e28343c6981ba523a365c56ec7f
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-08 15:51:11 +0530

    feat: wrap with snackbar provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import { HashRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
+import { App } from './App';
 import '../styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <SnackbarProvider maxSnack={2}>
+        <App />
+      </SnackbarProvider>
     </HashRouter>
   </React.StrictMode>
 );
