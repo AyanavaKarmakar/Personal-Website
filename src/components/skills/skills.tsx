3c5e0d2143995096271f6553d2526873002d23a3
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-08 18:26:55 +0530

    feat: setup conditional rendering

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { EchartsLoader } from './echartsLoader';
+import { EchartRosePieChart } from './echartRosePieChart';
 
 interface Props {
   darkMode: boolean;
@@ -6,9 +8,23 @@ interface Props {
 
 export const Skills = (props: Props) => {
   const { darkMode } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const DELAY_MS = 3000;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }, DELAY_MS);
+  }, []);
+
   return (
     <>
-      <EchartsLoader darkMode={darkMode} />
+      {isLoading === true && <EchartsLoader darkMode={darkMode} />}
+      {isLoading === false && <EchartRosePieChart />}
     </>
   );
 };
