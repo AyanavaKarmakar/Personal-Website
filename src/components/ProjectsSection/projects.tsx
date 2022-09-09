aaf48cef49acfc78aef6232fb26fdedee4ae8a55
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-09 12:10:06 +0530

    feat: basic layout

--- src/components/ProjectsSection/projects.tsx
+++ src/components/ProjectsSection/projects.tsx
@@ -9,20 +9,61 @@ import {
   Grid,
 } from '@mui/material';
 
+interface Props {
+  darkMode: boolean;
+}
+
 interface ProjectListTypes {
   projectTitle: string;
   projectDescription: string;
+  projectLink: string;
   projectImage: string;
   altProjectImageText: string;
   projectButtonText: string;
 }
 
-export const Projects = () => {
+/**
+ * Refer = https://mui.com/material-ui/react-card/#primary-action
+ */
+export const Projects = (props: Props) => {
+  const { darkMode } = props;
+
   const projectsList = [
     {
       projectTitle: 'Lizard',
       projectDescription:
         'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      projectLink: 'https://stackoverflow.com/',
+      projectImage:
+        'https://ayanava-karmakar.imgix.net/https%3A%2F%2Fraw.githubusercontent.com%2FAyanavaKarmakar%2Fimgix-source-assets%2Fmain%2FsiteIcon.png?s=b56a16a7886aaf99f639de88c3fcdc0b',
+      altProjectImageText: 'green iguana',
+      projectButtonText: 'Share',
+    },
+    {
+      projectTitle: 'Lizard',
+      projectDescription:
+        'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      projectLink: 'https://stackoverflow.com/',
+      projectImage:
+        'https://ayanava-karmakar.imgix.net/https%3A%2F%2Fraw.githubusercontent.com%2FAyanavaKarmakar%2Fimgix-source-assets%2Fmain%2FsiteIcon.png?s=b56a16a7886aaf99f639de88c3fcdc0b',
+      altProjectImageText: 'green iguana',
+      projectButtonText: 'Share',
+    },
+    {
+      projectTitle: 'Lizard',
+      projectDescription:
+        'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      projectLink: 'https://stackoverflow.com/',
+      projectImage:
+        'https://ayanava-karmakar.imgix.net/https%3A%2F%2Fraw.githubusercontent.com%2FAyanavaKarmakar%2Fimgix-source-assets%2Fmain%2FsiteIcon.png?s=b56a16a7886aaf99f639de88c3fcdc0b',
+      altProjectImageText: 'green iguana',
+      projectButtonText: 'Share',
+    },
+    {
+      projectTitle: 'Lizard',
+      projectDescription:
+        'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      projectLink: 'https://stackoverflow.com/',
       projectImage:
         'https://ayanava-karmakar.imgix.net/https%3A%2F%2Fraw.githubusercontent.com%2FAyanavaKarmakar%2Fimgix-source-assets%2Fmain%2FsiteIcon.png?s=b56a16a7886aaf99f639de88c3fcdc0b',
       altProjectImageText: 'green iguana',
@@ -31,18 +72,25 @@ export const Projects = () => {
   ];
 
   return (
-    <Grid container sx={{ my: 2, mx: 2 }}>
+    <Grid
+      container
+      spacing={0}
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '85vh' }}
+    >
       {projectsList.map((project: ProjectListTypes) => {
         const {
           projectTitle,
           projectDescription,
+          projectLink,
           projectImage,
           altProjectImageText,
           projectButtonText,
         } = project;
 
         return (
-          <Card sx={{ maxWidth: 345 }} key={projectTitle}>
+          <Card sx={{ maxWidth: 250, mx: 5 }} key={projectTitle}>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -60,7 +108,12 @@ export const Projects = () => {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
+              <Button
+                color={`${darkMode === true ? 'secondary' : 'primary'}`}
+                variant="outlined"
+                href={projectLink}
+                fullWidth
+              >
                 {projectButtonText}
               </Button>
             </CardActions>
