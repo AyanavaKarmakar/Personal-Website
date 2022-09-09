4e8b8637b14784b3b233fcbde93b5ab7e7b7d75e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-09 21:01:05 +0530

    feat: revert

--- src/components/ContactSection/contact.tsx
+++ src/components/ContactSection/contact.tsx
@@ -1,22 +1,3 @@
-import { FormControl, InputLabel, Input } from '@mui/material';
-
-/**
- * Refer = https://mui.com/material-ui/api/form-control/
- */
 export const Contact = () => {
-  return (
-    <FormControl sx={{ my: 2, mx: 1 }}>
-      <InputLabel htmlFor="emailId">Email ID</InputLabel>
-      <Input
-        required={true}
-        fullWidth={true}
-        autoFocus={true}
-        error={false}
-        color="primary"
-        id="emailId"
-        name="emailId"
-        placeholder={'Enter your Email Id here'}
-      />
-    </FormControl>
-  );
+  return <>Contact Form Placeholder</>;
 };
