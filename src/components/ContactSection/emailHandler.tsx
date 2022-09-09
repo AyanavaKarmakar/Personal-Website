c7f592ff233345c265767757954e6c61021ad9ad
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-10 03:33:54 +0530

    feat: setup basic usage

--- src/components/ContactSection/emailHandler.tsx
+++ src/components/ContactSection/emailHandler.tsx
@@ -1,12 +1,38 @@
+import { SMTPClient } from 'emailjs';
+
 interface Props {
   fullName: string;
   emailId: string;
   message: string;
 }
 
-export const EmailHandler = (props: Props) => {
+/**
+ * Refer = https://www.npmjs.com/package/emailjs
+ */
+
+const client = new SMTPClient({
+  user: 'user',
+  password: 'password',
+  host: 'smtp.your-email.com',
+  ssl: true,
+});
+
+export const EmailHandler = async (props: Props) => {
   const { fullName, emailId, message } = props;
   console.log(fullName);
   console.log(emailId);
   console.log(message);
+
+  try {
+    const message = await client.sendAsync({
+      text: 'i hope this works',
+      from: 'you <username@your-email.com>',
+      to: 'someone <someone@your-email.com>, another <another@your-email.com>',
+      cc: 'else <else@your-email.com>',
+      subject: 'testing emailjs',
+    });
+    console.log(message);
+  } catch (err) {
+    console.error(err);
+  }
 };
