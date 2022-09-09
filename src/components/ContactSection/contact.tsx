a66fe2fec6d3611906da93d164dda88bb12ccdb4
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-09 22:26:32 +0530

    feat: reorder folder structure

--- src/components/ContactSection/contact.tsx
+++ src/components/ContactSection/contact.tsx
@@ -1,60 +1,15 @@
-import { Stack, TextField, Typography } from '@mui/material';
+import { ContactForm } from './index';
 
 interface Props {
   darkMode: boolean;
 }
 
-/**
- * Refer: https://mui.com/material-ui/react-text-field/#components
- */
 export const Contact = (props: Props) => {
   const { darkMode } = props;
 
   return (
-    <Stack
-      spacing={2}
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: '85vh' }}
-      component="form"
-      noValidate
-      autoComplete="off"
-      sx={{ mx: 20 }}
-    >
-      <Typography
-        gutterBottom
-        variant="h2"
-        color={`${darkMode === true ? 'secondary' : 'primary'}`}
-      >
-        Contact Me!
-      </Typography>
-      <TextField
-        autoFocus
-        fullWidth
-        color={`${darkMode === true ? 'secondary' : 'primary'}`}
-        id="fullName"
-        label="Your Full Name"
-        variant="outlined"
-        placeholder="Enter your full name here"
-      />
-      <TextField
-        fullWidth
-        color={`${darkMode === true ? 'secondary' : 'primary'}`}
-        id="emailId"
-        label="Your Email"
-        variant="outlined"
-        placeholder="Enter your email id here"
-      />
-      <TextField
-        multiline
-        fullWidth
-        color={`${darkMode === true ? 'secondary' : 'primary'}`}
-        rows={10}
-        id="message"
-        label="Your Message"
-        variant="outlined"
-        placeholder="Enter your message here"
-      />
-    </Stack>
+    <>
+      <ContactForm darkMode={darkMode} />
+    </>
   );
 };
