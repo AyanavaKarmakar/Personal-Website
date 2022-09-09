8bdb258709da47b6742eef81b14402be6cb7aa50
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-09 10:54:46 +0530

    feat: map all projects v1

--- src/components/ProjectsSection/projects.tsx
+++ src/components/ProjectsSection/projects.tsx
@@ -9,33 +9,64 @@ import {
   Grid,
 } from '@mui/material';
 
+interface ProjectListTypes {
+  projectTitle: string;
+  projectDescription: string;
+  projectImage: string;
+  altProjectImageText: string;
+  projectButtonText: string;
+}
+
 export const Projects = () => {
+  const projectsList = [
+    {
+      projectTitle: 'Lizard',
+      projectDescription:
+        'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      projectImage:
+        'https://ayanava-karmakar.imgix.net/https%3A%2F%2Fraw.githubusercontent.com%2FAyanavaKarmakar%2Fimgix-source-assets%2Fmain%2FsiteIcon.png?s=b56a16a7886aaf99f639de88c3fcdc0b',
+      altProjectImageText: 'green iguana',
+      projectButtonText: 'Share',
+    },
+  ];
+
   return (
     <Grid container sx={{ my: 2, mx: 2 }}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://ayanava-karmakar.imgix.net/https%3A%2F%2Fraw.githubusercontent.com%2FAyanavaKarmakar%2Fimgix-source-assets%2Fmain%2FsiteIcon.png?s=b56a16a7886aaf99f639de88c3fcdc0b"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-        </CardActions>
-      </Card>
+      {projectsList.map((project: ProjectListTypes) => {
+        const {
+          projectTitle,
+          projectDescription,
+          projectImage,
+          altProjectImageText,
+          projectButtonText,
+        } = project;
+
+        return (
+          <Card sx={{ maxWidth: 345 }} key={projectTitle}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={projectImage}
+                alt={altProjectImageText}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {projectTitle}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {projectDescription}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                {projectButtonText}
+              </Button>
+            </CardActions>
+          </Card>
+        );
+      })}
     </Grid>
   );
 };
