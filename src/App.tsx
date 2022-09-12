cd0042b2886a3558ee4d57b5986b3d84d87f830e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-12 15:51:32 +0530

    feat: passed  darMode state as props

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,7 @@ export const App = () => {
             isViewedOnDesktop === true ? (
               <>
                 <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
-                <Extras />
+                <Extras darkMode={darkMode} />
               </>
             ) : (
               <ForceDesktopUnloading
