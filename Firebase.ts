ef060375c6c55eb5ced64e7e55bb51cf9c17e38a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-23 17:44:14 +0530

    feat: add firebase sdk

---
+++ Firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyANuRPcmvuI18MbTpmYaH2Q3uWXFjBpBEY',
+  authDomain: 'personal-website-88e33.firebaseapp.com',
+  projectId: 'personal-website-88e33',
+  storageBucket: 'personal-website-88e33.appspot.com',
+  messagingSenderId: '330722402333',
+  appId: '1:330722402333:web:58b90377153e4d183966f4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
