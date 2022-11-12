cf8bbd0592751ea4222f2fbfb291d011f587260e
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-13 02:12:48 +0530

    home page done

--- src/utils/HomeTexts.ts
+++ src/utils/HomeTexts.ts
@@ -2,8 +2,11 @@ import { z } from "zod";
 
 const HomeTextValidator = z.object({
   id: z.number().positive(),
-  heading: z.string().min(5),
-  para: z.string().min(10),
+  heading: z.string().min(5).optional(),
+  para1: z.string().min(10).optional(),
+  para2: z.string().min(10).optional(),
+  para3: z.string().min(10).optional(),
+  para4: z.string().min(10).optional(),
 });
 
 type HomeText = z.infer<typeof HomeTextValidator>;
@@ -12,6 +15,28 @@ export const HomeTexts: HomeText[] = [
   {
     id: 1,
     heading: "About Me",
-    para: "Frontend Developer with a passion for simple, responsive, and mobile-first cross-platform application development.",
+    para1:
+      "Frontend Developer with a passion for simple, responsive, and mobile-first cross-platform application development, and an Open Souce Contributor :)",
+  },
+  {
+    id: 2,
+    heading: "Work Experience",
+    para1: "1. Intern at GeeksForGeeks — 3 months (Current)",
+    para2: "2. Intern at Cognizant Technology Solutions — 6 months",
+  },
+  {
+    id: 3,
+    heading: "Education",
+    para1: "1. B.Tech in Electrical Engineering | GPA: 8.7 — (2018-2022)",
+    para2: "2. Higher Secondary | Stream: PCMCs | GPA: 9.2 — (2016-2018)",
+    para3: "3. Secondary | GPA: 9.3 — (2015-2016)",
+  },
+  {
+    id: 4,
+    heading: "Hobbies",
+    para1: "1. Aesthetic Photography ( Curious? Take a peek at ",
+    para2: "2. Open Source Contribution",
+    para3: "3. Cycling",
+    para4: "4. Swimming",
   },
 ];
