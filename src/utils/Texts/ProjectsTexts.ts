b86a48aaddc9dc71964156f47eccfee61d18f7ce
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-14 00:59:51 +0530

    hmm

--- src/utils/Texts/ProjectsTexts.ts
+++ src/utils/Texts/ProjectsTexts.ts
@@ -17,7 +17,31 @@ export const ProjectsTexts: ProjectText[] = [
     heading: "Guestbook",
     para1:
       "Guestbook is a simple web application that allows users to sign in, and leave a message. It is built using the T3 Stack (Next.js, TypeScript, and TailwindCSS).",
-    para2: "1. Open Source at GitHub",
-    para3: "2. Live at Vercel",
+    para2: "— Open Source @ GitHub",
+    para3: "— Live @ Vercel",
+  },
+  {
+    id: 2,
+    heading: "CatchApp",
+    para1:
+      "CatchApp is a real-time chat application that allows users to create rooms, and chat with other users. It is built using React Native, Expo, and Firebase.",
+    para2: "— Open Source @ GitHub",
+    para3: "— Live @ Expo",
+  },
+  {
+    id: 3,
+    heading: "ARID",
+    para1:
+      'ARID is a cross-platform application for checking "Dry Days" in India. It is built using React Native, Expo, and React Native Paper, and is available for both Android, and as a Web App.',
+    para2: "— Open Source @ GitHub",
+    para3: "— Live @ Vercel",
+  },
+  {
+    id: 4,
+    heading: "Portfolio",
+    para1:
+      "This is the portfolio website you are currently viewing. It is built using Astro, TypeScript, and TailwindCSS.",
+    para2: "— Open Source @ GitHub",
+    para3: "— Live @ Expo",
   },
 ];
