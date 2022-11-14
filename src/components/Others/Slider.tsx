b0c38c06b22c5e562ae01c1eab9e078ca1d80e41
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-14 17:55:32 +0530

    yes

--- src/components/Others/Slider.tsx
+++ src/components/Others/Slider.tsx
@@ -1,4 +1,4 @@
-import SimpleImageSlider from "react-simple-image-slider/dist/ImageSlider";
+import SimpleImageSlider from "react-simple-image-slider";
 
 const images = [
   { url: "https://picsum.photos/id/1018/1000/600/" },
@@ -8,14 +8,37 @@ const images = [
 
 export const Slider = () => {
   return (
-    <>
-      <SimpleImageSlider
-        width={896}
-        height={504}
-        images={images}
-        showBullets={true}
-        showNavs={true}
-      />
-    </>
+    <div className="flex items-center justify-center ml-3 mr-3 mt-5 mb-3">
+      <div className="visible lg:hidden">
+        <SimpleImageSlider
+          width={345}
+          height={275}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+          navStyle={3}
+          navSize={50}
+          navMargin={5}
+          loop={true}
+          autoPlay={true}
+          autoPlayDelay={3.0}
+        />
+      </div>
+      <div className="hidden lg:inline">
+        <SimpleImageSlider
+          width={800}
+          height={500}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+          navStyle={3}
+          navSize={50}
+          navMargin={5}
+          loop={true}
+          autoPlay={true}
+          autoPlayDelay={3.0}
+        />
+      </div>
+    </div>
   );
 };
